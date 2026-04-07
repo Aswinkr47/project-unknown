cdb1de1dca8e2e13390138d2dc2ad2bdcc6c3ee6
Aswinkr47 <aswin081210@gmail.com>
2026-04-07 21:08:40 +0530

    Add files via upload

---
+++ web.js
@@ -0,0 +1,15 @@
+function checkLogin()
+{
+	const correctusername= "cpboys";
+	const correctpassword="blublublu";
+	
+	let user =document.get("username").value;
+    let pass =document.get("password").value;
+if (user == correctusername && pass == correctpassword)
+{
+	window.location.href="webtest.html";
+}
+else {
+	document.getElementById("error")innerText="invalid username or password!";
+}
+}
