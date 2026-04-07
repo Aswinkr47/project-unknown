da102e70c6e076635f852324e49ee5f382b38b51
Aswinkr47 <aswin081210@gmail.com>
2026-04-07 21:52:21 +0530

    Update web.js

--- web.js
+++ web.js
@@ -3,11 +3,11 @@ function checkLogin()
 	const correctusername= "cpboys";
 	const correctpassword="123456";
 	
-	let user =document.get("username").value;
-    let pass =document.get("password").value;
+	let user =document.getElementById("username").value;
+    let pass =document.getElementById("password").value;
 if (user == correctusername && pass == correctpassword)
 {
-	window.location.href="home.html";
+	window.location.href= "home.html";
 }
 else {
 	document.getElementById("error")innerText="invalid username or password!";
